f8fc962af482b59eafc84a19b610f30c205f98ab
outsidev <seyburak@gmail.com>
2016-11-22 22:47:14 +0300

    Updated css and models, added comments page

--- subreddapp/models.py
+++ subreddapp/models.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-    username = models.ForeignKey('auth.User')
     text = models.TextField(default="")
     published_date = models.DateTimeField(default=timezone.now)
     points = models.PositiveIntegerField(default=0)
@@ -20,11 +19,13 @@ class Post(models.Model):
         return self.title
 
 class Post_Main(Post):
+    username = models.ForeignKey('auth.User', related_name="user_mainposts")
     link_adress = models.CharField(max_length=200)
     title =  models.CharField(max_length=100)
 
 class Post_Comment(Post):
-    parent_post = models.ForeignKey('Post_Main')
+    username = models.ForeignKey('auth.User', related_name="user_comments")
+    parent_post = models.ForeignKey('Post_Main',related_name="post_comments")
 
     def __str__(self):
         return self.text
--- subreddapp/views.py
+++ subreddapp/views.py
@@ -4,9 +4,10 @@ from .models import Post_Main,Post_Comment
 
 # Create your views here.
 def main_page(request):
-    posts = Post_Main.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post_Main.objects.all().order_by('-published_date')
     return render(request, 'sub/main_page.html', {'posts':posts})
 
 def post_comments(request, pk):
     post = get_object_or_404(Post_Main, pk=pk)
-    return render(request, 'sub/post_comments.html', {'post':post})
+    comments = post.post_comments.all()
+    return render(request, 'sub/post_comments.html', {'post':post,'comments':comments})
