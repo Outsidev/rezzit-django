2c85578cec803ee9e715abce691913732d602bf2
outsidev <seyburak@gmail.com>
2016-11-22 12:21:09 +0300

    Added new template and css files, Updated views and url

--- subreddapp/urls.py
+++ subreddapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_page, name="main_page"),
-    url(r'^comments/(?P<post_id>[0-9]+)/$', views.post_comments),
+    url(r'^comments/(?P<pk>[0-9]+)/$', views.post_comments, name="post_comments"),
 ]
--- subreddapp/views.py
+++ subreddapp/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
-from .models import Post_Main
+from .models import Post_Main,Post_Comment
 
 # Create your views here.
 def main_page(request):
     posts = Post_Main.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'sub/main_page.html', {'posts':posts})
 
-def post_comments(request):
-    return render(request, 'sub/post_comments.html', {})
+def post_comments(request, pk):
+    post = get_object_or_404(Post_Main, pk=pk)
+    return render(request, 'sub/post_comments.html', {'post':post})
